a911040ee66edc84500b81ab057d6f17393a88e8
ElkanaKorach <124582764+ElkanaKorach@users.noreply.github.com>
2024-03-09 22:41:13 +0100

    first commit
    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import telegram
+import logging
+import os
+import requests
+import json
+import time
+
+def main():
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+if __name__ == '__main__':
+    main()
