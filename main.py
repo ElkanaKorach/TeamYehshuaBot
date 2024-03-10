91229817b02ea4d6466b0e9ded4ea6c681c8050f
AdminOjoyeMarvin92 <57502197+AdminOjoyeMarvin92@users.noreply.github.com>
2024-03-10 05:02:49 +0100

    rec

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
-from telegram.ext import Updater, MessageHandler, Filters
+from os import name
+import asyncio
+from telegram import Update, Chat
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import logging
 from datetime import datetime
 
-# Ersetze 'YOUR_TOKEN_HERE' mit dem Token deines Bots
+# Ersetze '6942337491:AAGVKMvHayewt5CUcNFg8xx_zprobgJ4jak' mit dem Token deines Bots
 TOKEN = '6942337491:AAGVKMvHayewt5CUcNFg8xx_zprobgJ4jak'
 
 # Logging-Konfiguration
-# shellcheck disable=SC1065
-# shellcheck disable=SC1072
-# shellcheck disable=SC1073
-# shellcheck disable=SC1064
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Handler-Funktion, die bei jeder neuen Nachricht aufgerufen wird
-def echo(update, context):
-    chat_type = update.message.chat.type
-    chat_id = update.message.chat.id
-    user_name = update.message.from_user.name
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_type = update.effective_chat.type
+    chat_id = update.effective_chat.id
+    user_name = update.effective_user.name
     message_text = update.message.text
     message_date = update.message.date
-    reply_to_message = update.message.chat.location
+    reply_to_message = update.message.reply_to_message
 
-    logger.info(f"Nachricht von {user_name} im {chat_type} (ID: {chat_id}): '{message_text}', Zeit: {message_date}, {reply_to_message}")
+    logger.info(f"Nachricht von {user_name} im {chat_type} (ID: {chat_id}): '{message_text}', Zeit: {message_date}, Antwort auf Nachricht: {reply_to_message}")
 
-def main():
-    # Updater und Dispatcher initialisieren
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
+async def main() -> None:
+    # Erstelle eine Anwendung und gib dein Bot Token an
+    application = Application.builder().token(TOKEN).build()
 
-    # Handler, der auf alle Textnachrichten reagiert
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    # Füge einen MessageHandler hinzu, der auf alle Textnachrichten reagiert
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    # Startet den Bot
-    updater.start_polling()
-    updater.idle()
+    # Starte den Bot
+    await application.run_polling()
 
-if __name__ == '__main__':
-    main()
+if name == '__main__':
+    asyncio.run(main())
